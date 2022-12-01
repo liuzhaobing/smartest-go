bb3398ef5f2739850183389be1ee95635a553dea
sevel <sevel.liu@cloudminds.com>
2022-12-01 15:11:00 +0800

    1.fix(修改技能测试报告)

--- task/reporter_skill.go
+++ task/reporter_skill.go
@@ -8,25 +8,85 @@ import (
 )
 
 func (Skill *SkillTask) getResultSummary() string {
-	total, _ := models.ReporterDB.MongoCount(SkillResultsTable, bson.M{"job_instance_id": Skill.JobInstanceId})
-	fail, _ := models.ReporterDB.MongoCount(SkillResultsTable, bson.M{"job_instance_id": Skill.JobInstanceId, "is_pass": false})
+	// 标题
+	summary := fmt.Sprintf("%s %s\n", Skill.SkillConfig.TaskName, Skill.JobInstanceId)
+
+	// 耗时统计
 	costInfo, _ := models.ReporterDB.MongoAggregate(SkillResultsTable, []bson.M{
 		{"$match": bson.M{"job_instance_id": Skill.JobInstanceId}},
 		{"$group": bson.M{
-			"_id":     "$job_instance_id",
-			"maxCost": bson.M{"$max": "$edg_cost"},
-			"minCost": bson.M{"$min": "$edg_cost"},
-			"avgCost": bson.M{"$avg": "$edg_cost"},
+			"_id":      "$job_instance_id",
+			"max_cost": bson.M{"$max": "$edg_cost"},
+			"min_cost": bson.M{"$min": "$edg_cost"},
+			"avg_cost": bson.M{"$avg": "$edg_cost"},
 		}}})
-	summary := fmt.Sprintf("%s %s\n用例统计:%d, 错误数:%d, 正确率:%f, 用例并发数:%d\n最大耗时:%d, 最小耗时:%d, 平均耗时:%f\n请求地址:%s,AgentId:%d",
-		Skill.SkillConfig.TaskName,
-		Skill.JobInstanceId,
-		total, fail, 1-float32(fail)/float32(total), Skill.SkillConfig.ChanNum,
-		costInfo[0].Map()["maxCost"],
-		costInfo[0].Map()["minCost"],
-		costInfo[0].Map()["avgCost"],
-		Skill.SkillConfig.ConnAddr,
-		Skill.SkillConfig.AgentId)
+	summary += fmt.Sprintf("耗时统计:最大耗时:%d, 最小耗时:%d, 平均耗时:%.2f\n", costInfo[0].Map()["max_cost"], costInfo[0].Map()["min_cost"], costInfo[0].Map()["avg_cost"])
+
+	// 发布必测统计
+	isSmokeTotal, _ := models.ReporterDB.MongoCount(SkillResultsTable, bson.M{"job_instance_id": Skill.JobInstanceId, "is_smoke": 1})
+	if isSmokeTotal != 0 {
+		smokeIntentPass, _ := models.ReporterDB.MongoCount(SkillResultsTable, bson.M{"job_instance_id": Skill.JobInstanceId, "is_smoke": 1, "is_pass": true})
+		smokeParamPass, _ := models.ReporterDB.MongoCount(SkillResultsTable, bson.M{"job_instance_id": Skill.JobInstanceId, "is_smoke": 1, "param_info_is_pass": true})
+		summary += fmt.Sprintf("★★★发布必测用例总数:%d,错误数:%d,意图正确率:%f,槽位正确率:%f,错误数:%d\n",
+			isSmokeTotal,
+			isSmokeTotal-smokeIntentPass,
+			float32(smokeIntentPass)/float32(isSmokeTotal),
+			float32(smokeParamPass)/float32(isSmokeTotal),
+			isSmokeTotal-smokeParamPass)
+	}
+
+	// 查询版本信息
+	caseVersionInfo, _ := models.ReporterDB.MongoAggregate(SkillResultsTable, []bson.M{
+		{"$match": bson.M{"job_instance_id": Skill.JobInstanceId}},
+		{"$group": bson.M{"_id": bson.M{"case_version": "$case_version"}}},
+		{"$sort": bson.M{"_id.case_version": -1}},
+		{"$project": bson.M{"_id": 0, "case_version": "$_id.case_version"}},
+	})
+	FirstVersion := 0.0
+	SecondVersion := 0.0
+	if len(caseVersionInfo) > 1 {
+		if caseVersionInfo[0].Map()["case_version"] != nil {
+			FirstVersion, _ = (caseVersionInfo[0].Map()["case_version"]).(float64)
+		}
+		if caseVersionInfo[1].Map()["case_version"] != nil {
+			SecondVersion, _ = (caseVersionInfo[1].Map()["case_version"]).(float64)
+		}
+	}
+
+	// 最高版本统计
+	firstVersionTotal, _ := models.ReporterDB.MongoCount(SkillResultsTable, bson.M{"job_instance_id": Skill.JobInstanceId})
+	firstVersionIntentPass, _ := models.ReporterDB.MongoCount(SkillResultsTable, bson.M{"job_instance_id": Skill.JobInstanceId, "is_pass": true})
+	firstVersionParamPass, _ := models.ReporterDB.MongoCount(SkillResultsTable, bson.M{"job_instance_id": Skill.JobInstanceId, "param_info_is_pass": true})
+	firstVersionRegexCount, _ := models.ReporterDB.MongoCount(SkillResultsTable, bson.M{"job_instance_id": Skill.JobInstanceId, "algo": "regex"})
+	firstVersionSystemCount, _ := models.ReporterDB.MongoCount(SkillResultsTable, bson.M{"job_instance_id": Skill.JobInstanceId, "act_source": "system_service"})
+	summary += fmt.Sprintf("用例版本:%.2f,用例总数:%d,错误数:%d,意图正确率:%f,槽位正确率:%f,意图支撑中算法占比%f,工程模板占比%f\n",
+		FirstVersion,
+		firstVersionTotal,
+		firstVersionTotal-firstVersionIntentPass,
+		float32(firstVersionIntentPass)/float32(firstVersionTotal),
+		float32(firstVersionParamPass)/float32(firstVersionTotal),
+		1-float32(firstVersionRegexCount)/float32(firstVersionSystemCount),
+		float32(firstVersionRegexCount)/float32(firstVersionSystemCount))
+
+	// 第二版本统计
+	if SecondVersion != 0.0 {
+		secondVersionTotal, _ := models.ReporterDB.MongoCount(SkillResultsTable, bson.M{"case_version": bson.M{"$lte": SecondVersion}, "job_instance_id": Skill.JobInstanceId})
+		secondVersionIntentPass, _ := models.ReporterDB.MongoCount(SkillResultsTable, bson.M{"case_version": bson.M{"$lte": SecondVersion}, "job_instance_id": Skill.JobInstanceId, "is_pass": true})
+		secondVersionParamPass, _ := models.ReporterDB.MongoCount(SkillResultsTable, bson.M{"case_version": bson.M{"$lte": SecondVersion}, "job_instance_id": Skill.JobInstanceId, "param_info_is_pass": true})
+		secondVersionRegexCount, _ := models.ReporterDB.MongoCount(SkillResultsTable, bson.M{"case_version": bson.M{"$lte": SecondVersion}, "job_instance_id": Skill.JobInstanceId, "algo": "regex"})
+		secondVersionSystemCount, _ := models.ReporterDB.MongoCount(SkillResultsTable, bson.M{"case_version": bson.M{"$lte": SecondVersion}, "job_instance_id": Skill.JobInstanceId, "act_source": "system_service"})
+		summary += fmt.Sprintf("用例版本:%.2f,用例总数:%d,错误数:%d,意图正确率:%f,槽位正确率:%f,意图支撑中算法占比%f,工程模板占比%f\n",
+			SecondVersion,
+			secondVersionTotal,
+			secondVersionTotal-secondVersionIntentPass,
+			float32(secondVersionIntentPass)/float32(secondVersionTotal),
+			float32(secondVersionParamPass)/float32(secondVersionTotal),
+			1-float32(secondVersionRegexCount)/float32(secondVersionSystemCount),
+			float32(secondVersionRegexCount)/float32(secondVersionSystemCount))
+	}
+
+	// 附加信息
+	summary += fmt.Sprintf("请求参数:请求地址:%s,AgentId:%d,并发数:%d\n", Skill.SkillConfig.ConnAddr, Skill.SkillConfig.AgentId, Skill.SkillConfig.ChanNum)
 	return summary
 }
 

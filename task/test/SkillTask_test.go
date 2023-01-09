d99a5e0c9847e7af3d1ea41b32ca9705e98109f8
sevel <zhaobinng.liu@outlook.com>
2023-01-09 22:10:26 +0800

    1.fix(新增多字段打印)

--- task/test/SkillTask_test.go
+++ task/test/SkillTask_test.go
@@ -8,6 +8,7 @@ import (
 	"google.golang.org/grpc"
 	"smartest-go/proto/talk"
 	"testing"
+	"time"
 )
 
 func Test_svTalkOnce(t *testing.T) {
@@ -24,28 +25,30 @@ func Test_svTalkOnce(t *testing.T) {
 		Position:   "104.061;30.5444",
 		Version:    "v3",
 		IsHa:       false,
-		TestMode:   true,
+		TestMode:   false,
 		Asr:        talk.Asr{Lang: "ZH"},
 	}
-	addr := "172.16.32.2:32247" //m8地址
-	//addr := "172.16.23.85:30811"  //fit地址
+	//addr := "172.16.32.2:32247" //m8地址
+	addr := "172.16.23.85:30811" //fit地址
 	request.RobotID = "5C1AEC03573747D"
 	request.EnvInfo["devicetype"] = "ginger"
 	request.InputContext = ""
-	request.Asr.Text = "现在几点了"
+	request.Asr.Text = "你会跳什么舞"
 
 	conn, _ := grpc.Dial(addr, grpc.WithInsecure())
+	startTime := time.Now()
 	Resp, err := svTalkOnce(conn, request)
+	var duration time.Duration
+	duration = time.Now().Sub(startTime)
 	if err != nil {
 		fmt.Println(err.Error())
 		panic(err)
 	}
 
-	fmt.Println(fmt.Sprintf(`{"Cost":%dms,"Source":"%s","Domain":"%s","Intent":"%s"}`,
+	fmt.Println(fmt.Sprintf(`{"edg_cost":%d,"third_cost":%d,"intent":"%s"}`,
+		duration.Milliseconds(),
 		Resp.Cost,
-		Resp.Source,
-		Resp.HitLog.Fields["domain"].GetStringValue(),
-		Resp.HitLog.Fields["intent"].GetStringValue(),
+		Resp.Source+"/"+Resp.HitLog.Fields["domain"].GetStringValue()+"/"+Resp.HitLog.Fields["intent"].GetStringValue(),
 	))
 	for _, info := range Resp.Tts {
 		fmt.Println(fmt.Sprintf(`{"Answer":"%s","PicUrl":"%s","VideoUrl":"%s","AudioUrl":"%s"}`,
@@ -53,6 +56,14 @@ func Test_svTalkOnce(t *testing.T) {
 			info.Action.Param.PicUrl,
 			info.Action.Param.VideoUrl,
 			info.Action.Param.Url,
+		))
+
+		fmt.Println(fmt.Sprintf(`{"name":"%s","domain":"%s","intent":"%s","params":%s,"row_data":%s}`,
+			info.Action.Name,
+			info.Action.Param.Domain,
+			info.Action.Param.Intent,
+			info.Action.Param.Params,
+			info.Action.Param.RawData,
 		))
 	}
 	if Resp.HitLog.Fields["paraminfo"] != nil {
